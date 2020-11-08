f8c4996411360f3be7063226c071a74558599aa3
Ken Howard <ken@kenhoward.dev>
2020-11-08 12:38:22 -0800

    fea: add image replacement

---
+++ README.md
@@ -0,0 +1,7 @@
+shipit
+======
+
+:shipit: shipit is a tool for software developers to add shipit squirrel images
+to code comments.
+
+![DEMO](shipit-demo.gif)
---
+++ src/content.js
@@ -0,0 +1,9 @@
+const injectScript = (src, node) => {
+  const [container] = document.getElementsByTagName(node);
+  const script = document.createElement('script');
+  script.setAttribute('type', 'text/javascript');
+  script.id = 'shipit';
+  script.src = src;
+  container.appendChild(script);
+};
+injectScript(browser.extension.getURL('src/shipit.js'), 'body');
