6137f5843e5c06ada125f4d5e0f8d243c6f4bdfd
Ken Howard <ken@kenhoward.dev>
2020-12-16 16:02:33 -0800

    fix: always include protocol

--- src/shipit.ts
+++ src/shipit.ts
@@ -1,21 +1,28 @@
 (() => {
   const search = /!shipit/gi
-  const getShipitImage = (): string => {
+  let nextImageId = 0
+  const getShipitImage = (imageId: number): string => {
     const squirrels = [
-      '//shipitsquirrel.github.io/images/ship%20it%20squirrel.png',
-      '//64.media.tumblr.com/tumblr_lybw63nzPp1r5bvcto1_500.jpg',
-      '//i.imgur.com/DPVM1.png',
-      '//d2f8dzk2mhcqts.cloudfront.net/0772_PEW_Roundup/09_Squirrel.jpg',
-      '//www.cybersalt.org/images/funnypictures/s/supersquirrel.jpg',
-      '//www.zmescience.com/wp-content/uploads/2010/09/squirrel.jpg',
-      '//1.bp.blogspot.com/_v0neUj-VDa4/TFBEbqFQcII/AAAAAAAAFBU/E8kPNmF1h1E/s640/squirrelbacca-thumb.jpg',
+      'https://shipitsquirrel.github.io/images/ship%20it%20squirrel.png',
+      'https://64.media.tumblr.com/tumblr_lybw63nzPp1r5bvcto1_500.jpg',
+      'https://i.imgur.com/DPVM1.png',
+      'https://d2f8dzk2mhcqts.cloudfront.net/0772_PEW_Roundup/09_Squirrel.jpg',
+      'https://www.cybersalt.org/images/funnypictures/s/supersquirrel.jpg',
+      'https://www.zmescience.com/wp-content/uploads/2010/09/squirrel.jpg',
+      'https://1.bp.blogspot.com/_v0neUj-VDa4/TFBEbqFQcII/AAAAAAAAFBU/E8kPNmF1h1E/s640/squirrelbacca-thumb.jpg',
+      'https://media.giphy.com/media/10vk5L8tHKN1UQ/giphy.gif',
+      'https://media.giphy.com/media/19uUmES4K72KI/giphy.gif',
     ]
 
-    const random = Math.floor(Math.random() * Math.floor(squirrels.length - 1))
-    return squirrels[random]
+    if (squirrels[imageId + 1] !== undefined) {
+      nextImageId = imageId + 1
+    } else {
+      nextImageId = 0
+    }
+    return squirrels[imageId]
   }
   window.addEventListener('keyup', () => {
-    const markdownImg = `![SHIPIT](${getShipitImage()})`
+    const markdownImg = `![SHIPIT](${getShipitImage(nextImageId)})`
     const textAreas = document.getElementsByTagName('textarea')
     for (const t of textAreas) {
       const v = t.value
